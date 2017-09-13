03a81131d2485df7bfec1dc200e68732853d662d
Jane Ossai <buff_jane@hotmail.com>
2017-09-13 02:50:22 +0100

    major tech and ui improvements imported semantuc ui to improve ui - i like it better than bootstrap added skills section improved portfolio with cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  //skills: Array<String>;
+  skills: string[] = ['Angular 1,2 & 4', 'Java', 'Spring Boot', 'HTML','CSS', 'JS', 'Cloud application development', 'SQL', 'MSSQL', 'MongoDB', 'MEAN Stack', 'Ionic Framework', 'Agile Application Development'
+  ,'Unity', 'Python', 'Communication', 'Teamwoerk', 'Presentation Skills', 'Customer Service'];
 }

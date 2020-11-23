d4c50882cb3f57d9eb0e5e774b8435f396451ea0
Jane Ossai <buff_jane@hotmail.com>
2020-11-23 06:16:14 +0000

    Add back to top button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
   techSkills: string[] = ['JavaScript (ES6)', 'Typescript', 'HTML', 'CSS',
     'Angular2+', 'VueJS', 'AngularJS', 'React', 'Java', 'Spring Boot',
@@ -15,4 +15,25 @@ export class AppComponent {
   softSkills: string[] = ['Software engineering', 'Frontend development', 'Backend development', 'Problem-solving'];
 
   skills: string[] = this.techSkills.concat(this.softSkills);
+
+  ngOnInit(): void {
+    // Get the button
+    const mybutton = document.getElementById('myBtn');
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction(); };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = 'block';
+      } else {
+        mybutton.style.display = 'none';
+      }
+    }
+  }
+
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 };
